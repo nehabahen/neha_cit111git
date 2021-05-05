4774455978bda3e170a90869e19c9823ea20b031
nehabahen <80130219+nehabahen@users.noreply.github.com>
2021-05-05 17:01:02 -0400

    Add files via upload

---
+++ PrintVerticalLine.java
@@ -0,0 +1,14 @@
+package project;
+
+import java.util.TimerTask;
+
+public class PrintVerticalLine extends TimerTask {
+
+	@Override
+	public void run() {
+		System.out.print(" |");
+		
+	}
+	
+	
+}
